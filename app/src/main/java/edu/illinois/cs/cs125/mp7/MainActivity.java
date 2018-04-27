7e4089e3fd6f67df2213b95ade9742a0d4dadf86
jcerra2 <jcerra2@illinois.edu>
2018-04-27 16:38:34 -0500

    Merge branch 'bs7' of https://github.com/bowenbs7/MP7 into jcerra2
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7/MainActivity.java
@@ -6,7 +6,14 @@ import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -19,11 +26,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    final Button seeResults = (Button) findViewById(R.id.seeResults);
-    public void seeResults(View v) {
-
-    }
-
-    // Hello
+    //Using volley to get the JSON data from pricecharting.com
+    String url = "https://www.pricecharting.com/api/product?t=e858b303b42db5af194bd8dc53bdfcf5e098ff42";
+    final TextView mTextView = (TextView) findViewById(R.id.textView_1);
+    RequestQueue queue = Volley.newRequestQueue(this);
+    StringRequest jsonResult = new StringRequest(Request.Method.GET, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    // Display the first 500 characters of the response string.
+                    mTextView.setText("Response is: "+ response.substring(0,500));
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            mTextView.setText("That didn't work!");
+        }
+    });
 }
